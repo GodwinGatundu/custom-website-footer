edd3cd76578533d3174e1b3b4ecd78f94d3f589d
Godwin Gatundu <gatundugodwin5@gmail.com>
2019-02-11 16:40:28 +0300

    Initial commit

---
+++ homepage.php
@@ -0,0 +1,124 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> -->
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <title>Predominant Writers | WOrking?</title>
+
+
+	<!-- Bootstrap CSS -->
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
+	<style type="text/css">
+    @import url("custom.css");
+    @import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');;
+    </style>
+</head>
+<body>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="#"><img src="img/logo.png"></img></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"><span class="navbar-toggler-icon"></span></button>
+		<div class="collapse navbar-collapse" id="navbarResponsive">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">About</a>
+				</li><li class="nav-item">
+					<a class="nav-link" href="#">Services</a>
+				</li><li class="nav-item">
+					<a class="nav-link" href="#">Contact Us</a>
+				</li>
+				</li><li class="nav-item">
+					<a class="nav-link" href="#">Place Order</a>
+				</li>				
+			</ul>
+		</div>
+	</div>
+</nav>
+
+
+
+<!--- Jumbotron -->
+
+<!--- Two Column Section -->
+<div class="container-fluid padding">
+	<div class="row padding">
+		<div class="col-md-12 col-lg-6">
+			<h2>World Class Writers</h2>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+			<br>
+			<a href="#"><button class="btn btn-primary">Learn More</button></a>
+		</div>
+		<div class="col-lg-6">
+			<img src="img/writer2.jpg" class="img-fluid">
+		</div>
+	</div>
+</div>
+
+<!--- Fixed background -->
+<figure>
+	<div class="fixed-wrap">
+		<div><img id="fixed" src="img/mac.png"></div>		
+	</div>
+</figure>
+
+
+<!-- Footer -->
+    <div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                	<div class="col-md-5 item text">
+                        <h3>Predominant Writers</h3>
+                        <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. 
+                        Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. 
+                    Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                    </div>
+                    <div class="col-md-1 item text"></div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>What We Do</h3>
+                        <ul>
+                            <li><a href="#">Essay Writing</a></li>
+                            <li><a href="#">Research Papers</a></li>
+                            <li><a href="#">Dissertation Writing</a></li>
+                            <li><a href="#">Thesis Writing</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Quick Links</h3>
+                        <ul>
+                            <li><a href="#">How to make an order</a></li>
+                            <li><a href="#">Our pricing</a></li>
+                            <li><a href="#">Support page</a></li>
+                            <li><a href="#">Getting started</a></li>
+                        </ul>
+                    </div>                    
+                    <div class="col item social"><ul class="list-unstyled list-inline social text-center">
+						<li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-facebook"></i></a></li>
+						<li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-twitter"></i></a></li>
+						<li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-instagram"></i></a></li>
+						<li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-google-plus"></i></a></li>
+						<li class="list-inline-item"><a href="javascript:void();" target="_blank"><i class="fa fa-envelope"></i></a></li>
+					</ul></div>
+                </div>
+                <p class="copyright">Predominant Writers © 2018</p>
+            </div>
+        </footer>
+    </div>
+<!-- ./Footer -->
+</body>
+</html>
