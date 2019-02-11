77eda808567752810e84e1222715cd2be8a3012d
Godwin Gatundu <gatundugodwin5@gmail.com>
2019-02-11 16:44:14 +0300

    Changes to homepage.php

--- homepage.php
+++ homepage.php
@@ -1,89 +1,27 @@
-<?php 
-
-?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> -->
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <title>Predominant Writers | WOrking?</title>
+    <title>Company name</title>
 
-
-	<!-- Bootstrap CSS -->
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
 	<style type="text/css">
     @import url("custom.css");
     @import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');;
     </style>
 </head>
 <body>
-
-<!-- Navigation -->
-<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
-	<div class="container-fluid">
-		<a class="navbar-brand" href="#"><img src="img/logo.png"></img></a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"><span class="navbar-toggler-icon"></span></button>
-		<div class="collapse navbar-collapse" id="navbarResponsive">
-			<ul class="navbar-nav ml-auto">
-				<li class="nav-item active">
-					<a class="nav-link" href="#">Home</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">About</a>
-				</li><li class="nav-item">
-					<a class="nav-link" href="#">Services</a>
-				</li><li class="nav-item">
-					<a class="nav-link" href="#">Contact Us</a>
-				</li>
-				</li><li class="nav-item">
-					<a class="nav-link" href="#">Place Order</a>
-				</li>				
-			</ul>
-		</div>
-	</div>
-</nav>
-
-
-
-<!--- Jumbotron -->
-
-<!--- Two Column Section -->
-<div class="container-fluid padding">
-	<div class="row padding">
-		<div class="col-md-12 col-lg-6">
-			<h2>World Class Writers</h2>
-			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-			<br>
-			<a href="#"><button class="btn btn-primary">Learn More</button></a>
-		</div>
-		<div class="col-lg-6">
-			<img src="img/writer2.jpg" class="img-fluid">
-		</div>
-	</div>
-</div>
-
-<!--- Fixed background -->
-<figure>
-	<div class="fixed-wrap">
-		<div><img id="fixed" src="img/mac.png"></div>		
-	</div>
-</figure>
-
-
 <!-- Footer -->
     <div class="footer-dark">
         <footer>
             <div class="container">
                 <div class="row">
                 	<div class="col-md-5 item text">
-                        <h3>Predominant Writers</h3>
+                        <h3>Company name</h3>
                         <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. 
                         Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. 
                     Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
@@ -92,10 +30,10 @@
                     <div class="col-sm-6 col-md-3 item">
                         <h3>What We Do</h3>
                         <ul>
-                            <li><a href="#">Essay Writing</a></li>
-                            <li><a href="#">Research Papers</a></li>
-                            <li><a href="#">Dissertation Writing</a></li>
-                            <li><a href="#">Thesis Writing</a></li>
+                            <li><a href="#">Our Company</a></li>
+                            <li><a href="#">Our Suppliers</a></li>
+                            <li><a href="#">Advertise With Us</a></li>
+                            <li><a href="#">Careers</a></li>
                         </ul>
                     </div>
                     <div class="col-sm-6 col-md-3 item">
@@ -115,7 +53,7 @@
 						<li class="list-inline-item"><a href="javascript:void();" target="_blank"><i class="fa fa-envelope"></i></a></li>
 					</ul></div>
                 </div>
-                <p class="copyright">Predominant Writers © 2018</p>
+                <p class="copyright">Company name © 2018</p>
             </div>
         </footer>
     </div>
